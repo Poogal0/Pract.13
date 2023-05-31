4ac494f813a2d7badeeaf95cef86d3bc7e39fc63
Poogal0 <golubchikov_2002@mail.ru>
2023-05-31 18:50:13 +0400

    Task 2

--- main.cpp
+++ main.cpp
@@ -1,30 +1,29 @@
-#include <QtWidgets>
-#include <QDebug>
-#include <QFile>
+#include <QApplication>
+#include <QSlider>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QDateTime>
 
-class MainWindow:public QMainWindow {
-  public:
-  MainWindow();
-};
-
-MainWindow::MainWindow() {
-int widthWindow=700;
-int heightWindow=200;
-qDebug()<<widthWindow<<heightWindow;
-QFile file("config.ini");
-file.open(QFile::ReadOnly);
-QString strWidth, strHeight;
-strWidth=file.readLine();
-strHeight=file.readLine();
-qDebug()<<strWidth<<strHeight;
-file.close();
-widthWindow=strWidth.toInt();
-heightWindow=strHeight.toInt();
-resize(widthWindow,heightWindow);
-}
 int main(int argc, char *argv[]) {
-  QApplication app(argc,argv);
-  MainWindow mainWin;
-  mainWin.show();
-  return app.exec();
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Slider Date Example");
+
+    QSlider slider(Qt::Horizontal);
+    QLabel label(QDateTime::currentDateTime().toString("dd.MM.yyyy"));
+
+    QVBoxLayout layout;
+    layout.addWidget(&slider);
+    layout.addWidget(&label);
+    window.setLayout(&layout);
+
+    QObject::connect(&slider, &QSlider::valueChanged, [&label](int value) {
+        QDateTime date = QDateTime::currentDateTime().addDays(value);
+        label.setText(date.toString("dd.MM.yyyy"));
+    });
+
+    window.show();
+
+    return app.exec();
 }
