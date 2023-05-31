1fce8419a100a7793a3b39cdc899ca199a267822
Poogal0 <golubchikov_2002@mail.ru>
2023-05-31 19:06:30 +0400

    v1 - created empty app

--- main.cpp
+++ main.cpp
@@ -1,29 +1,8 @@
 #include <QApplication>
-#include <QSlider>
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QDateTime>
-
+#include <QWidget>
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
     QWidget window;
-    window.setWindowTitle("Slider Date Example");
-
-    QSlider slider(Qt::Horizontal);
-    QLabel label(QDateTime::currentDateTime().toString("dd.MM.yyyy"));
-
-    QVBoxLayout layout;
-    layout.addWidget(&slider);
-    layout.addWidget(&label);
-    window.setLayout(&layout);
-
-    QObject::connect(&slider, &QSlider::valueChanged, [&label](int value) {
-        QDateTime date = QDateTime::currentDateTime().addDays(value);
-        label.setText(date.toString("dd.MM.yyyy"));
-    });
-
     window.show();
-
     return app.exec();
 }
